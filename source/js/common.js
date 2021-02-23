cc193072d8859367e866f31a054ed62f118ec5d5
Andrey <soinproduction@gmail.com>
2021-02-24 00:50:09 +0200

    выполнен хедер страницы about

--- source/js/common.js
+++ source/js/common.js
@@ -191,33 +191,20 @@ $(function () {
     }
 
 
+    $('.tab').on('click', function (e) {
+      e.preventDefault();
+
+      $($(this).siblings()).removeClass('tab_active');
+      $($(this).closest('.tabs-wrapper').siblings().find('div')).removeClass('tabs-content_active');
+
+      $(this).addClass('tab_active');
+      $($(this).attr('href')).addClass('tabs-content_active');
+    });
+
+
+
 
 
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
